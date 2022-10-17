01107bd2503e25b437fbc26ae93106984d472e5b
nil-5da3 <n.petruccioli@gmail.com>
2022-10-17 15:43:48 +0200

    added check allowed token

--- x/slashrefund/keeper/msg_server_deposit.go
+++ x/slashrefund/keeper/msg_server_deposit.go
@@ -2,17 +2,34 @@ package keeper
 
 import (
 	"context"
+	"strings"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	"github.com/made-in-block/slash-refund/x/slashrefund/types"
 )
 
 // Manages the deposit of funds from a user to a particular validator into a module KVStore.
-// TODO: add param for allowed tokens.
-// TODO: check if allowed token.
+
 // TODO: check if  valid validator.
 func (k msgServer) Deposit(goCtx context.Context, msg *types.MsgDeposit) (*types.MsgDepositResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	// check if allowed token
+	var isAcceptable bool // default is false
+	for _, validToken := range strings.Split(k.AllowedTokens(ctx), ",") {
+		if msg.Amount.Denom == validToken {
+			isAcceptable = true
+			break
+		}
+	}
+	if !isAcceptable {
+		return nil, sdkerrors.Wrapf(
+			sdkerrors.ErrInvalidRequest, "invalid coin denomination: got %s. Allowed tokens are %s", msg.Amount.Denom, k.AllowedTokens(ctx),
+		)
+	}
+
+	// deposit coins
 
 	sender, _ := sdk.AccAddressFromBech32(msg.Creator)
 
