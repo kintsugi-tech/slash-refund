2ac85acf07c27178bb32740df0aa934cc63c5756
nil-5da3 <n.petruccioli@gmail.com>
2022-10-24 17:19:13 +0200

    initialized addEntry function

--- x/slashrefund/keeper/unbonding_deposit.go
+++ x/slashrefund/keeper/unbonding_deposit.go
@@ -1,6 +1,10 @@
 package keeper
 
 import (
+	"time"
+
+	"cosmossdk.io/math"
+
 	"github.com/cosmos/cosmos-sdk/store/prefix"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/made-in-block/slash-refund/x/slashrefund/types"
@@ -19,15 +23,15 @@ func (k Keeper) SetUnbondingDeposit(ctx sdk.Context, unbondingDeposit types.Unbo
 // GetUnbondingDeposit returns a unbondingDeposit from its index
 func (k Keeper) GetUnbondingDeposit(
 	ctx sdk.Context,
-	delegatorAddress string,
-	validatorAddress string,
+	delegatorAddress sdk.AccAddress,
+	validatorAddress sdk.ValAddress,
 
 ) (val types.UnbondingDeposit, found bool) {
 	store := prefix.NewStore(ctx.KVStore(k.storeKey), types.KeyPrefix(types.UnbondingDepositKeyPrefix))
 
 	b := store.Get(types.UnbondingDepositKey(
-		delegatorAddress,
-		validatorAddress,
+		delegatorAddress.String(),
+		validatorAddress.String(),
 	))
 	if b == nil {
 		return val, false
@@ -65,4 +69,39 @@ func (k Keeper) GetAllUnbondingDeposit(ctx sdk.Context) (list []types.UnbondingD
 	}
 
 	return
+}
+
+// SetUnbondingDepositEntry adds an entry to the unbonding deposit at
+// the given addresses. It creates the unbonding deposit if it does not exist.
+func (k Keeper) SetUnbondingDepositEntry(
+	ctx sdk.Context, delegatorAddr sdk.AccAddress, validatorAddr sdk.ValAddress,
+	creationHeight int64, minTime time.Time, balance math.Int,
+) types.UnbondingDeposit {
+	ubd, found := k.GetUnbondingDeposit(ctx, delegatorAddr, validatorAddr)
+	if found {
+		ubd.AddEntry(creationHeight, minTime, balance)
+	} else {
+		ubd = types.NewUnbondingDeposit(delegatorAddr, validatorAddr, creationHeight, minTime, balance)
+	}
+
+	k.SetUnbondingDeposit(ctx, ubd)
+
+	return ubd
+}
+
+// InsertUBDQueue inserts an unbonding deposit to the appropriate timeslice
+// in the unbonding queue.
+func (k Keeper) InsertUBDQueue(ctx sdk.Context, ubd types.UnbondingDeposit, completionTime time.Time) {
+
+	// TODO scaffold DVPair
+
+	dvPair := types.DVPair{DelegatorAddress: ubd.DelegatorAddress, ValidatorAddress: ubd.ValidatorAddress}
+
+	timeSlice := k.GetUBDQueueTimeSlice(ctx, completionTime)
+	if len(timeSlice) == 0 {
+		k.SetUBDQueueTimeSlice(ctx, completionTime, []types.DVPair{dvPair})
+	} else {
+		timeSlice = append(timeSlice, dvPair)
+		k.SetUBDQueueTimeSlice(ctx, completionTime, timeSlice)
+	}
 }
---
+++ x/slashrefund/types/unbonding_deposit.go
@@ -0,0 +1,55 @@
+package types
+
+import (
+	"time"
+
+	"cosmossdk.io/math"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+// AddEntry - append entry to the unbonding deposit
+func (ubd *UnbondingDeposit) AddEntry(creationHeight int64, minTime time.Time, balance math.Int) {
+	// Check the entries exists with creation_height and complete_time
+	entryIndex := -1
+	for index, ubdEntry := range ubd.Entries {
+		if ubdEntry.CreationHeight == creationHeight && ubdEntry.CompletionTime.Equal(minTime) {
+			entryIndex = index
+			break
+		}
+	}
+	// entryIndex exists
+	if entryIndex != -1 {
+		ubdEntry := ubd.Entries[entryIndex]
+		ubdEntry.Balance = ubdEntry.Balance.Add(balance)
+		ubdEntry.InitialBalance = ubdEntry.InitialBalance.Add(balance)
+
+		// update the entry
+		ubd.Entries[entryIndex] = ubdEntry
+	} else {
+		// append the new unbond deposit entry
+		entry := NewUnbondingDepositEntry(creationHeight, minTime, balance)
+		ubd.Entries = append(ubd.Entries, entry)
+	}
+}
+
+func NewUnbondingDepositEntry(creationHeight int64, completionTime time.Time, balance math.Int) UnbondingDepositEntry {
+	return UnbondingDepositEntry{
+		CreationHeight: creationHeight,
+		CompletionTime: completionTime,
+		InitialBalance: balance,
+		Balance:        balance,
+	}
+}
+
+func NewUnbondingDeposit(
+	delegatorAddr sdk.AccAddress, validatorAddr sdk.ValAddress,
+	creationHeight int64, minTime time.Time, balance math.Int,
+) UnbondingDeposit {
+	return UnbondingDeposit{
+		DelegatorAddress: delegatorAddr.String(),
+		ValidatorAddress: validatorAddr.String(),
+		Entries: []UnbondingDepositEntry{
+			NewUnbondingDepositEntry(creationHeight, minTime, balance),
+		},
+	}
+}
