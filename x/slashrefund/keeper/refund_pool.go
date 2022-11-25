600dcd8e472f70c6b3a2ba45a5669809bcdb5879
nil-5da3 <n.petruccioli@gmail.com>
2022-11-25 17:13:57 +0100

    added refund pool methods

--- x/slashrefund/keeper/refund_pool.go
+++ x/slashrefund/keeper/refund_pool.go
@@ -66,3 +66,61 @@ func (k Keeper) GetAllRefundPool(ctx sdk.Context) (list []types.RefundPool) {
 
 	return
 }
+
+func (k Keeper) AddRefPoolTokensAndShares(
+	ctx sdk.Context,
+	refundPool types.RefundPool,
+	tokensToAdd sdk.Coin,
+) (addedShares sdk.Dec) {
+
+	var issuedShares sdk.Dec
+	if refundPool.Shares.IsZero() {
+		issuedShares = sdk.NewDecFromInt(tokensToAdd.Amount)
+	} else {
+		// TODO: we have to manage post slashing send of tokens. We have to put zero shares when  tokens -> 0
+		shares, err := refundPool.SharesFromTokens(tokensToAdd)
+		if err != nil {
+			panic(err)
+		}
+		issuedShares = shares
+
+	}
+
+	refundPool.Tokens = refundPool.Tokens.Add(tokensToAdd)
+	refundPool.Shares = refundPool.Shares.Add(issuedShares)
+
+	k.SetRefundPool(ctx, refundPool)
+
+	return issuedShares
+}
+
+func (k Keeper) RemoveRefPoolTokensAndShares(
+	ctx sdk.Context,
+	refundPool types.RefundPool,
+	sharesToRemove sdk.Dec,
+) (types.RefundPool, sdk.Int) {
+
+	var issuedTokensAmt sdk.Int
+
+	remainingShares := refundPool.Shares.Sub(sharesToRemove)
+
+	if remainingShares.IsZero() {
+		// last share gets any trimmings
+		issuedTokensAmt = refundPool.Tokens.Amount
+		// TODO: generalize it considering AllowedTokens param
+		refundPool.Tokens.Amount = sdk.ZeroInt()
+	} else {
+		// leave excess tokens in the deposit pool
+		// however fully use all the depositor shares
+		issuedTokensAmt = refundPool.TokensFromShares(sharesToRemove).TruncateInt()
+		refundPool.Tokens.Amount = refundPool.Tokens.Amount.Sub(issuedTokensAmt)
+		if refundPool.Tokens.Amount.IsNegative() {
+			panic("attempting to remove more tokens than available in validator")
+		}
+	}
+
+	refundPool.Shares = remainingShares
+	k.SetRefundPool(ctx, refundPool)
+
+	return refundPool, issuedTokensAmt
+}
