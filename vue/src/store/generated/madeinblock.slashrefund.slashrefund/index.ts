49bd25051353a7cb4c83a68c75b4ccc431104394
nil-5da3 <n.petruccioli@gmail.com>
2022-12-05 14:02:08 +0100

    added claim message

--- vue/src/store/generated/madeinblock.slashrefund.slashrefund/index.ts
+++ vue/src/store/generated/madeinblock.slashrefund.slashrefund/index.ts
@@ -482,6 +482,19 @@ export default {
 				}
 			}
 		},
+		async sendMsgClaim({ rootGetters }, { value, fee = [], memo = '' }) {
+			try {
+				const client=await initClient(rootGetters)
+				const result = await client.MadeinblockSlashrefundSlashrefund.tx.sendMsgClaim({ value, fee: {amount: fee, gas: "200000"}, memo })
+				return result
+			} catch (e) {
+				if (e == MissingWalletError) {
+					throw new Error('TxClient:MsgClaim:Init Could not initialize signing client. Wallet is required.')
+				}else{
+					throw new Error('TxClient:MsgClaim:Send Could not broadcast Tx: '+ e.message)
+				}
+			}
+		},
 		
 		async MsgDeposit({ rootGetters }, { value }) {
 			try {
@@ -506,6 +519,19 @@ export default {
 					throw new Error('TxClient:MsgWithdraw:Init Could not initialize signing client. Wallet is required.')
 				} else{
 					throw new Error('TxClient:MsgWithdraw:Create Could not create message: ' + e.message)
+				}
+			}
+		},
+		async MsgClaim({ rootGetters }, { value }) {
+			try {
+				const client=initClient(rootGetters)
+				const msg = await client.MadeinblockSlashrefundSlashrefund.tx.msgClaim({value})
+				return msg
+			} catch (e) {
+				if (e == MissingWalletError) {
+					throw new Error('TxClient:MsgClaim:Init Could not initialize signing client. Wallet is required.')
+				} else{
+					throw new Error('TxClient:MsgClaim:Create Could not create message: ' + e.message)
 				}
 			}
 		},
--- x/slashrefund/module_simulation.go
+++ x/slashrefund/module_simulation.go
@@ -32,6 +32,10 @@ const (
 	// TODO: Determine the simulation weight value
 	defaultWeightMsgWithdraw int = 100
 
+	opWeightMsgClaim = "op_weight_msg_claim"
+	// TODO: Determine the simulation weight value
+	defaultWeightMsgClaim int = 100
+
 	// this line is used by starport scaffolding # simapp/module/const
 )
 
@@ -90,6 +94,17 @@ func (am AppModule) WeightedOperations(simState module.SimulationState) []simtyp
 	operations = append(operations, simulation.NewWeightedOperation(
 		weightMsgWithdraw,
 		slashrefundsimulation.SimulateMsgWithdraw(am.accountKeeper, am.bankKeeper, am.keeper),
+	))
+
+	var weightMsgClaim int
+	simState.AppParams.GetOrGenerate(simState.Cdc, opWeightMsgClaim, &weightMsgClaim, nil,
+		func(_ *rand.Rand) {
+			weightMsgClaim = defaultWeightMsgClaim
+		},
+	)
+	operations = append(operations, simulation.NewWeightedOperation(
+		weightMsgClaim,
+		slashrefundsimulation.SimulateMsgClaim(am.accountKeeper, am.bankKeeper, am.keeper),
 	))
 
 	// this line is used by starport scaffolding # simapp/module/operation
